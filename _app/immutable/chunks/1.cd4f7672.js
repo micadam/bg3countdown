9dedb7940279f42bd7569b18b9bbb79750b45c7f
Adam Michalski <contact@adammi.ch>
2023-08-03 11:09:05 +0100

    Updates

---
+++ _app/immutable/chunks/1.cd4f7672.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.70a000df.js";export{t as component};
--- _app/immutable/chunks/2.dd24ea0a.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.27669d82.js";export{t as component};
